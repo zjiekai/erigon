c99f3ab42147db24120af4b0ff83362c9ac00652
alex.sharov <AskAlexSharov@gmail.com>
2021-08-31 09:23:32 +0700

    pool: correct use of baseFee

--- p2p/server.go
+++ p2p/server.go
@@ -854,7 +854,7 @@ func (srv *Server) addPeerChecks(peers map[enode.ID]*Peer, inboundCount int, c *
 	// Drop connections with no matching protocols.
 	if len(srv.Protocols) > 0 && countMatchingProtocols(srv.Protocols, c.caps) == 0 {
 		for i := range c.caps {
-			log.Warn("reject protocol", "found", c.caps[i].Name, "found", c.caps[i].Version, "name", c.caps[i].Name)
+			log.Warn("reject protocol", "found", c.caps[i].Name, "found", c.caps[i].Version, "name", c.Name)
 		}
 		return DiscUselessPeer
 	}
